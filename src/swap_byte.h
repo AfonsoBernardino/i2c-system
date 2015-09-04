11ddb8815a38dbf2e3b4aaf2f1536b68e89596dd
AfonsoBernardino <afonso_bernardino@outlook.com>
2015-09-04 10:55:27 +0100

    removed some unnecessary files

--- src/swap_byte.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SWAP_BYTE_H__
-#define __SWAP_BYTE_H__
-static inline __u16 SWAP_BYTE(int word){ //Swap high byte with low byte
-	return ((word&0x00ff)<<8)|(word>>8); //(SMBus standard sends low byte first)
-} 
-#endif
